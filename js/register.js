3ecd013b444fee67202a7893cb46d991f6a3f839
simoncct1 <simoncouclet@gmail.com>
2024-04-19 17:36:10 +0200

    rip

--- js/register.js
+++ js/register.js
@@ -1,14 +1,18 @@
-const apiUrl = 'http://localhost:8000/foo';
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// const thisForm = document.getElementById('userinfo');
+// thisForm.addEventListener('submit', async function (e) {
+//     e.preventDefault();
+//     const formData = new FormData(thisForm).entries();
+//     for (var [key, value] of formData.entries()) { 
+//       console.log(key, value);
+//     }
+//     const response = await fetch('http://127.0.0.1:8000/register', {
+//         method: 'POST',
+//         headers: {  
+//           Accept: "application/json"  
+//         }  ,
+//         body: JSON.stringify(Object.fromEntries(formData))
+//     });
+
+//     const result = await response.json();
+//     console.log(result)
+// });
