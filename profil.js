83c57e56dd7e96d39d85fa04724498e1089fa57d
simoncct1 <simoncouclet@gmail.com>
2024-05-06 16:49:20 +0200

    petits changes

--- profil.js
+++ profil.js
@@ -21,6 +21,39 @@ fetch(url, {
 		Accept: "application/json",
         'Authorization': 'Bearer ' + access_token,  
     }
-}).then(res => res.json().then(console.log)).catch(console.error); 
+})  .then(response => response.json())
+.then(data => {
+    console.log(data);
+    document.getElementById("pp").setAttribute('src','/avatars/' + data.image);
+    document.getElementById("prenom").innerHTML = data.name;
+    document.getElementById("ndc").innerHTML = data.ndc;
+    document.getElementById("email").innerHTML = data.email;
+  })
+.catch(error => console.error('Error:', error));
 
+document.getElementById("imgfrm").addEventListener("click", function(){
+    document.getElementById("avatar").style.display ='flex'
+})
+
+
+document.getElementById("upp").addEventListener("change", function() {
+    changeImage(this);
+  });
+  
+  function changeImage(input) {
+    var reader;
+  
+    if (input.files && input.files[0]) {
+      reader = new FileReader();
+  
+      reader.onload = function(e) {
+        console.log(input.value);
+        console.log(e.target.result)
+        document.getElementById("pp").setAttribute('src', e.target.result);
+      }
+  
+      reader.readAsDataURL(input.files[0]);
+
+    }
+  }
 
